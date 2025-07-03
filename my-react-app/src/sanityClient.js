0d82484e270e0fc9c5a7a46741197048b57ad135
Vladlena <cravcenco97@inbox.ru>
2025-07-03 23:21:00 +0300

    fix

--- my-react-app/src/sanityClient.js
+++ my-react-app/src/sanityClient.js
@@ -1,8 +1,15 @@
 import { createClient } from '@sanity/client'
+import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
   projectId: '3iowxmww',      // ← из https://sanity.io/manage
   dataset: 'production',
   apiVersion: '2023-01-01',
   useCdn: true
-})
+});
+
+const builder = imageUrlBuilder(client);
+
+export function urlFor(source) {
+  return builder.image(source);
+}
