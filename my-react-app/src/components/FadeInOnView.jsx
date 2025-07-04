a64e43e8b0f24ffe58fe003e89264e639e50f220
Vladlena <cravcenco97@inbox.ru>
2025-07-04 20:24:29 +0300

    Update FadeInOnView.jsx

--- my-react-app/src/components/FadeInOnView.jsx
+++ my-react-app/src/components/FadeInOnView.jsx
@@ -1,20 +1,39 @@
-// src/components/FadeInOnView.jsx
 import React from "react";
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
-const FadeInOnView = ({ children, delay = 0, x = 0, y = 30, duration = 0.8 }) => {
+const FadeInOnView = ({
+  children,
+  className = "",
+  delay = 0,
+  x = 0,
+  y = 30,
+  duration = 0.8,
+}) => {
+  const controls = useAnimation();
   const [ref, inView] = useInView({
     triggerOnce: true,
-    threshold: 0.5,
+    threshold: 0.5, // 50% элемента в зоне видимости
   });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start({
+        opacity: 1,
+        x: 0,
+        y: 0,
+        transition: { duration, delay },
+      });
+    }
+  }, [controls, inView, delay, duration]);
 
   return (
     <motion.div
       ref={ref}
+      className={className}
       initial={{ opacity: 0, x, y }}
-      animate={inView ? { opacity: 1, x: 0, y: 0 } : {}}
-      transition={{ duration, delay }}
+      animate={controls}
     >
       {children}
     </motion.div>
