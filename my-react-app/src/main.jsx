c6b7c179ee1346cd736dbc53eadf938ae13d5602
Vladlena <cravcenco97@inbox.ru>
2025-07-08 14:15:02 +0300

    preload

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import { LanguageProvider } from './context/LanguageContext';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { LanguageProvider } from "./context/LanguageContext";
 
-createRoot(document.getElementById('root')).render(
+window.addEventListener("load", () => {
+  const preloader = document.getElementById("preloader");
+  if (preloader) {
+    preloader.classList.add("fade-out");
+    setTimeout(() => preloader.remove(), 400); // ждать пока закончится transition
+  }
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <LanguageProvider>
       <App />
