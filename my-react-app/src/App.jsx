176c106366c2b3cc4c59639f5ed24a79ae4687f5
Vladlena <cravcenco97@inbox.ru>
2025-07-14 20:25:46 +0300

    clean

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import WhoWeAre from './components/WhoWeAre';
-import WhereWeOperate from './components/WhereWeOperate';
-import WhatWeDo from './components/WhatWeDo';
-import EnergySection from './components/EnergySection';
-import FooterMap from './components/FooterMap';
-import Footer from './components/Footer';
-import CopyFooter from './components/CopyFooter';
-import Team from './components/Team';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import WhoWeAre from "./components/WhoWeAre";
+import WhereWeOperate from "./components/WhereWeOperate";
+import WhatWeDo from "./components/WhatWeDo";
+import EnergySection from "./components/EnergySection";
+import FooterMap from "./components/FooterMap";
+import Footer from "./components/Footer";
+import CopyFooter from "./components/CopyFooter";
+import Team from "./components/Team";
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
       <Header />
       <Hero />
       <WhoWeAre />
-        <Team />
+      <Team />
       <WhereWeOperate />
       <WhatWeDo />
       <EnergySection />
--- my-react-app/src/sanityClient.js
+++ my-react-app/src/sanityClient.js
@@ -5,7 +5,8 @@ export const client = createClient({
   projectId: '3iowxmww',      // ← из https://sanity.io/manage
   dataset: 'production',
   apiVersion: '2023-01-01',
-  useCdn: true
+  useCdn: true,
+  token: import.meta.env.VITE_SANITY_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
