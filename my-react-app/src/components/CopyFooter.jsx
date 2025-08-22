acf1c9a9b2fe06e15b851e8b6570162138844baa
Vladlena <cravcenco97@inbox.ru>
2025-08-22 11:32:53 +0300

    Update CopyFooter.jsx

--- my-react-app/src/components/CopyFooter.jsx
+++ my-react-app/src/components/CopyFooter.jsx
@@ -6,7 +6,7 @@ export default function CopyFooter() {
     <section className='Copy'>
         <div className="footer-container">
          <div className="footer-bottom">
-        <p>Copyright © 2025. Designed By <a href="https://instagram.com/mornin.digital" target="_blank" rel="noopener noreferrer">Mornin’ Digital</a></p>
+        <p>Copyright © 2025. Designed By <a href="https://www.instagram.com/mornin_digital?igsh=MTJzaTlhdWg1djVweA==" target="_blank" rel="noopener noreferrer">Mornin’ Digital</a></p>
         <a href="#top" className="scroll-top"><svg width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M7.00062 2.94086L2.06062 7.88086L0.640625 6.48086L7.00062 0.12086L13.3606 6.48086L11.9406 7.88086L7.00062 2.94086Z" fill="#10171E"/>
 </svg>
